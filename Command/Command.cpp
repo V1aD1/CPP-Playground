f77e661cdc922ac72c2761ec7b7699a199c3d1e3
Vlad Baderca <v_baderca@wargaming.net>
2020-11-27 11:36:42 +0100

    added undo() to Command

--- Command/Command.cpp
+++ Command/Command.cpp
@@ -10,16 +10,21 @@ namespace CommandNamespace {
 		int m_balance = 0;
 		int m_overdraftLimit = -500;
 
-		void deposit(int amount) {
+		bool deposit(int amount) {
 			m_balance += amount;
 			cout << "Deposited " << amount << "$ into account, balance is now: " << m_balance << "$\n";
+
+			return true;
 		}
 
-		void withdraw(int amount) {
+		bool withdraw(int amount) {
 			if (m_balance - amount > m_overdraftLimit) {
 				m_balance -= amount;
 				cout << "Withdrew " << amount << "$ from account, balance is now: " << m_balance << "$\n";
+				return true;
 			}
+
+			return false;
 		}
 
 		friend ostream& operator<<(ostream& os, const BankAccount& account) {
@@ -30,12 +35,14 @@ namespace CommandNamespace {
 
 	struct Command {
 		virtual void call() = 0;
+		virtual void undo() = 0;
 	};
 
 	struct BankAccountCommand : Command {
 		BankAccount& m_account;
 		enum Action {deposit, withdraw} m_action;
 		int m_amount;
+		bool m_succeeded = false;
 
 		BankAccountCommand(BankAccount& account, Action action, int amount):
 			m_account(account), m_action(action), m_amount(amount){}
@@ -43,10 +50,25 @@ namespace CommandNamespace {
 		void call() override {
 			switch (m_action) {
 			case deposit:
-				m_account.deposit(m_amount);
+				m_succeeded = m_account.deposit(m_amount);
 				break;
 			case withdraw:
+				m_succeeded = m_account.withdraw(m_amount);
+				break;
+			}
+		}
+
+		void undo() override {
+			if (!m_succeeded)
+				return;
+			switch (m_action) {
+			case deposit:
 				m_account.withdraw(m_amount);
+				m_succeeded = false;
+				break;
+			case withdraw:
+				m_account.deposit(m_amount);
+				m_succeeded = false;
 				break;
 			}
 		}
@@ -64,6 +86,9 @@ void CommandExample() {
 	};
 	for (auto& cmd : commands)
 		cmd.call();
+
+	for (auto it = commands.rbegin(); it != commands.rend(); ++it)
+		it->undo();
 
 	cout << acc << endl;
 }
